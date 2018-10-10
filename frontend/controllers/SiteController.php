62ee923f2ce399a9231254dd978a8b942da4012f
sun <sunhongenhotmail.com>
2018-10-10 10:23:22 +0800

    3

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -20,11 +20,11 @@ class SiteController extends Controller
 		$weixin = \Yii::$app->weixin;
 
 		$weixin->getMessageTxtArr();
-		
+		\Yii::info("发送的数据:".file_get_contents("php://input"));
 		if (!$weixin->MsgType){
 			return \Yii::$app->request->get('echostr');
 		}
-		\Yii::info("发送的数据:".file_get_contents("php://input"));
+		
 		$ResponseText = $weixin->getResponse()->getResponseText();
 		$ResponseText->Content = "MsgType:".$weixin->MsgType." Event:".$weixin->Event;
 		$resStr = $ResponseText->getResponse();
